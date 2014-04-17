4c0bf117177505cf4408af01f08dc403ada58aca
Joey Clouvel <joey@clouvel.com>
2014-04-17 02:25:34 +0100

    Initialise game

--- Game.cpp
+++ Game.cpp
@@ -1,14 +1,77 @@
 #include <stdio.h>
 #include "Game.h"
+#include "Crab.h"
+#include "Octopus.h"
+#include "Squid.h"
+#include "Spaceship.h"
 
 using namespace std;
 
 
 Game::Game() {
-	this->reset();
+	// Load all resources
+	this->player = IMG_Load("res/img/player.png");
+	this->crab = IMG_Load("res/img/crab.png");
+	this->octopus = IMG_Load("res/img/octopus.png");
+	this->squid = IMG_Load("res/img/squid.png");
+	this->spaceship = IMG_Load("res/img/spaceship.png");
+	
+	this->active = false;
+}
+
+
+Game::~Game() {
+	// Free all resources
+	SDL_FreeSurface(this->crab);
+	SDL_FreeSurface(this->octopus);
+	SDL_FreeSurface(this->squid);
+	SDL_FreeSurface(this->spaceship);
 }
 
 void Game::reset() {
-	this->level = 0;
+	this->level = 1;
 	this->score = 0;
+	this->life = 3;
+	this->active = true;
+	this->attackerPosition.x = 80;
+	this->attackerPosition.y = 150;
+	this->playerPosition.x = 100;
+	this->playerPosition.y = 0;
+	
+	// Clear an eventual previous array of attackers
+	this->attacker.clear();
+	
+	// Populate all new attackers
+	// Insert all squids
+	for(int i=0; i<11; i++) {
+		Squid squid = Squid(i, 0, this);
+		this->attacker.push_back(squid);
+	}
+	
+	// Then all crabs
+	for(int i=0; i<11; i++) {
+		for(int j=1; j<3; j++) {
+			Crab crab = Crab(i, j, this);
+			this->attacker.push_back(crab);
+		}
+	}
+	
+	// And finally, all octopuses
+	for(int i=0; i<11; i++) {
+		for(int j=3; j<5; j++) {
+			Octopus octopus = Octopus(i, j, this);
+			this->attacker.push_back(octopus);
+		}
+	}
+}
+
+void Game::decreaseLife() {
+	if(--this->life==0) {
+		// TODO: implement end of game
+		this->active = false;
+	}
+}
+
+void Game::move(int shift) {
+	this->playerPosition.x += shift;
 }
--- Game.h
+++ Game.h
@@ -1,19 +1,33 @@
 #include <vector>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
 #include "Attacker.h"
 
 #ifndef DEF_GAME
 #define DEF_GAME
 
+	struct Position {
+		int x;
+		int y;
+	};
+
 	class Game {
 		public:
 			int score;
 			int level;
+			int life;
+			bool active;
+			SDL_Surface *player, *crab, *octopus, *squid, *spaceship;
 			std::vector<Attacker> attacker;
+			Position attackerPosition, playerPosition;
 			
 			Game();
+			~Game();
 			void reset();
 			void updateScore();
 			void updateLevel();
+			void decreaseLife();
+			void move(int shift);
 			void tick();
 			int getSpeedFactor();
 	};
